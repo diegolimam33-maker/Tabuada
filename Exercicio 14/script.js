6812e4ace4ed78788f02203dcd6acf21c4d22d43
Diego Miranda <diegolimam33@gmail.com>
2026-06-09 15:26:49 -0300

    Adicionando Exercicio 14

---
+++ Exercicio 14/script.js
@@ -0,0 +1,24 @@
+
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia 
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#b8d2cf'
+    } else if (hora >= 12 && hora < 18) {
+        //Boa tarde
+        img.src = 'fototarde.png'
+        document.body.style.background = '#dca93a'
+    } else {
+        //Boa noite
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#1a1917'
+    }
+}
+
+
